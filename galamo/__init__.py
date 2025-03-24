7ec26b2c1cae1c867366d6ea71c095401a759c0d
Jashanpreet Singh Dingra <astrodingra@Jashanpreets-MacBook-Air.local>
2025-03-25 00:42:04 +0530

    Fixed sync issues and updated project

--- galamo/__init__.py
+++ galamo/__init__.py
@@ -3,8 +3,11 @@ import tensorflow as tf
 from .model import galaxy_morph, preprocess_image
 from . import bpt  # Import the BPT module
 
+__version__ = "0.0.4"  # Define the package version
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Suppress TensorFlow warnings
 tf.get_logger().setLevel('ERROR')  # Suppress other TensorFlow logs
+
 # Get the absolute model path inside the package
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "model.keras")
 
