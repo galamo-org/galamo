2584198f90aef7bb9e938207b5472bbc1a46cddb
Jashanpreet Singh Dingra <astrodingra@Jashanpreets-MacBook-Air.local>
2025-03-25 14:57:54 +0530

    Updated setup.py to 0.0.6

--- setup.py
+++ setup.py
@@ -6,16 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="galamo",
-    version="0.0.5",
-    author="Jashanpreet Singh Dingra",
-    author_email="astrodingra@gmail.com",
-    description="A Python package for comprehensive galaxy analysis, integrating machine learning and statistical methods. It provides automated tools for morphology classification, kinematics, photometry, and spectral analysis to aid astrophysical research.",
-    long_description=long_description,  # Use the content from README.md
-    long_description_content_type="text/markdown",  # Markdown format
-    url="https://github.com/jdingra11/galamo", 
-    include_package_data=True,  # ✅ Ensure package data is included
-    package_data={"galamo": ["model.keras", "encoder.pkl"]},  # ✅ Specify extra files
+    name="galamo", version="0.0.6", author="Jashanpreet Singh Dingra", 
+    author_email="astrodingra@gmail.com", description="A Python package 
+    for comprehensive galaxy analysis, integrating machine learning and 
+    statistical methods. It provides automated tools for morphology 
+    classification, kinematics, photometry, and spectral analysis to aid 
+    astrophysical research.", long_description=long_description, # Use the 
+    content from README.md long_description_content_type="text/markdown", 
+    # Markdown format url="https://github.com/jdingra11/galamo", 
+    include_package_data=True, # ✅ Ensure package data is included 
+    package_data={"galamo": ["model.keras", "encoder.pkl"]}, # ✅ Specify 
+    extra files
    
     install_requires=[
         "tensorflow",
