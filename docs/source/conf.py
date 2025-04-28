a6030bd090d2c5aca29a5cbdb05276807607390e
jsdingra11 <jsdingra@gmail.com>
2025-04-29 01:41:30 +0530

    rtd updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -43,12 +43,6 @@ html_theme_options = {
             "icon": "fab fa-github",
             "type": "fontawesome",
         },
-        {
-            "name": "X (Twitter)",
-            "url": "https://x.com/galamo_org",  # Replace with your X.com handle
-            "icon": "fab fa-x-twitter",         # FontAwesome updated icon for X
-            "type": "fontawesome",
-        },
     ],
 }
 
