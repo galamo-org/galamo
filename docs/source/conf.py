ffe3f854135695b7b4147d49f418f7554c1d72a4
jsdingra11 <jsdingra@gmail.com>
2025-04-29 01:40:12 +0530

    rtd updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,6 +23,35 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-html_logo = "_static/galamo_main.svg"
+
 html_theme = 'pydata_sphinx_theme'  # You can switch to 'sphinx_rtd_theme' later if you want
 html_static_path = ['_static']
+
+
+html_theme_options = {
+    "logo": {
+        "image_light": "_static/galamo_main.svg",   # Optional: Your logo path
+        "image_dark": "_static/gray-logo.svg",
+    },
+    "navbar_start": ["navbar-logo"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["search-field", "theme-switcher", "navbar-icon-links"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/galamo-org/galamo",  # Replace with your repo
+            "icon": "fab fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "X (Twitter)",
+            "url": "https://x.com/galamo_org",  # Replace with your X.com handle
+            "icon": "fab fa-x-twitter",         # FontAwesome updated icon for X
+            "type": "fontawesome",
+        },
+    ],
+}
+
+html_sidebars = {
+    "**": [],
+}
